9432293a37fe69ba4eacd2d86507e151e6c6656a
Ryan Teng <ryanrteng@gmail.com>
2019-04-06 20:12:18 -0400

    Added Box Collider

--- Engine/include/BoxCollider.h
+++ Engine/include/BoxCollider.h
@@ -1,9 +1,22 @@
 #ifndef FINALPROJECT_ARCUBED_BOXCOLLIDER_H
 #define FINALPROJECT_ARCUBED_BOXCOLLIDER_H
 
-
+/**
+ * A BoxCollider object. Has a check collision function which returns true if this block is overlapping with the given
+ * block.
+ */
 class BoxCollider {
+public:
+    BoxCollider(float x, float y, float width, float height);
+    bool checkCollision(float x2, float y2, float width2, float height2);
+    void setWidth(float w);
+    void setHeight(float h);
+    void shiftPosition(float x, float y);
 
+
+
+private:
+    float x, y, width, height;
 };
 
 
--- Engine/src/BoxCollider.cpp
+++ Engine/src/BoxCollider.cpp
@@ -1,1 +1,16 @@
 #include "BoxCollider.h"
+
+BoxCollider::BoxCollider(float x, float y, float width, float height): x(x), y(y), width(width), height(height) {};
+
+/**
+ * Collision function. Currently assumes that x and y are at the top left of the sprite
+ *
+ * @param x2
+ * @param y2
+ * @param width2
+ * @param height2
+ * @return
+ */
+BoxCollider::checkCollision(float x2, float y2, float width2, float height2) {
+    return x < x2 + width2 && x + width > x2 && y < y2 + height2 && y + height > y2;
+}
