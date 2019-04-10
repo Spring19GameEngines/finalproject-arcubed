ac57ee23a49b2f730b933feca30bcefce3052f4e
Andrew Alcala <andrewalc98@gmail.com>
2019-04-10 16:12:19 -0400

    Added RM to repo

---
+++ Engine/include/ResourceManager.h
@@ -0,0 +1,65 @@
+#ifndef RESOURCEMANAGER
+#define RESOURCEMANAGER
+
+#if defined(LINUX) || defined(MINGW)
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#else
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#endif
+
+#include <memory>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+/*! \brief Manages resources and ensures objects are only loaded once.
+ * Provides support for loading textures, fonts, text, background music, and
+ * sound effects
+ */
+class ResourceManager {
+ public:
+  //! Gets singleton instance
+  static ResourceManager &getInstance();
+  //! Completes any necessary startup logic for manager
+  void startUp();
+  //! Frees any resources acquired using manager
+  void shutDown();
+  //! Loads texture from map and saves to map if loaded for the first time
+  SDL_Texture *loadTexture(const std::string &path, SDL_Renderer *ren);
+  //! Loads font from map and saves to map if loaded for the first time
+  TTF_Font *loadFont(const std::string &path, int size);
+  /*!
+   * Uses provided path and size to load font
+   * Will try to load text from map if already created previously
+   * Returns tuple with texture as well as width and height of the created
+   * texture
+   */
+  std::tuple<SDL_Texture *, int, int> loadTextTexture(const std::string &path,
+                                                      int size,
+                                                      const std::string &text,
+                                                      SDL_Color color,
+                                                      SDL_Renderer *ren);
+  //! Loads sfx from map and saves to map if loaded for the first time
+  Mix_Chunk *loadEffect(const std::string &path);
+  //! Loads music from map and saves to map if loaded for the first time
+  Mix_Music *loadMusic(const std::string &path);
+
+ private:
+  ResourceManager();                         // Private Singleton
+  ResourceManager(ResourceManager const &);  // Avoid copy constructor
+  void operator=(ResourceManager const &);   // Don't allow assignment.
+
+  std::unordered_map<std::string, SDL_Texture *> textureMap;
+  std::unordered_map<std::string, TTF_Font *> fontMap;
+  std::unordered_map<std::string, std::tuple<SDL_Texture *, int, int>> textMap;
+  std::unordered_map<std::string, Mix_Chunk *> effectMap;
+  std::unordered_map<std::string, Mix_Music *> songMap;
+};
+
+#endif
---
+++ Engine/src/ResourceManager.cpp
@@ -0,0 +1,171 @@
+#include "ResourceManager.h"
+
+ResourceManager::ResourceManager() {}
+
+ResourceManager &ResourceManager::getInstance() {
+  static ResourceManager instance;
+  return instance;
+}
+
+void ResourceManager::startUp() {}
+
+void ResourceManager::shutDown() {
+  for (auto &t : textureMap) {
+    if (t.second) {
+      SDL_Log("Freeing texture %s", t.first.c_str());
+      SDL_DestroyTexture(t.second);
+      t.second = nullptr;
+    }
+  }
+
+  for (auto &f : fontMap) {
+    if (f.second) {
+      SDL_Log("Freeing font %s", f.first.c_str());
+      TTF_CloseFont(f.second);
+      f.second = nullptr;
+    }
+  }
+
+  for (auto &t : textMap) {
+    SDL_Log("Freeing text for %s", t.first.c_str());
+    if (std::get<0>(t.second)) {
+      SDL_DestroyTexture(std::get<0>(t.second));
+    }
+  }
+
+  for (auto &e : effectMap) {
+    if (e.second) {
+      SDL_Log("Freeing sound effect %s", e.first.c_str());
+      Mix_FreeChunk(e.second);
+    }
+  }
+
+  for (auto &s : songMap) {
+    if (s.second) {
+      SDL_Log("Freeing music track %s", s.first.c_str());
+      Mix_FreeMusic(s.second);
+    }
+  }
+}
+
+SDL_Texture *ResourceManager::loadTexture(const std::string &path,
+                                          SDL_Renderer *ren) {
+  auto storedTexture = textureMap.find(path);
+  if (storedTexture != textureMap.end()) {
+    return storedTexture->second;
+  }
+
+  SDL_Texture *newTexture = NULL;
+  SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+  if (loadedSurface == NULL) {
+    SDL_Log("Failed to allocate surface: %s", path.c_str());
+  } else {
+    // Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface(ren, loadedSurface);
+    if (newTexture == NULL) {
+      SDL_Log("Unable to create texture from %s! SDL Error: %s\n", path.c_str(),
+              SDL_GetError());
+    }
+    // Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
+  }
+
+  // Update map with texture
+  textureMap[path] = newTexture;
+
+  return newTexture;
+}
+
+TTF_Font *ResourceManager::loadFont(const std::string &path, int size) {
+  auto key = path + std::to_string(size);
+  auto storedFont = fontMap.find(key);
+  if (storedFont != fontMap.end()) {
+    return storedFont->second;
+  }
+
+  // Open the font
+  TTF_Font *loadedFont = TTF_OpenFont(path.c_str(), size);
+  if (loadedFont == NULL) {
+    SDL_Log("Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
+  }
+
+  fontMap[key] = loadedFont;
+
+  return loadedFont;
+}
+
+std::string colorAsString(SDL_Color &color) {
+  return "{" + std::to_string(color.r) + "," + std::to_string(color.g) + "," +
+         std::to_string(color.b) + "," + std::to_string(color.a) + "}";
+}
+
+std::tuple<SDL_Texture *, int, int> ResourceManager::loadTextTexture(
+    const std::string &path, int size, const std::string &text, SDL_Color color,
+    SDL_Renderer *ren) {
+  // TODO: maybe find a better way to store already created texts
+  auto key = path + "," + std::to_string(size) + "," + text + "," +
+             colorAsString(color);
+  auto storedText = textMap.find(key);
+  if (storedText != textMap.end()) {
+    return storedText->second;
+  }
+
+  TTF_Font *font = loadFont(path, size);
+  SDL_Texture *newTexture = NULL;
+  int w;
+  int h;
+  SDL_Surface *textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
+  if (textSurface == NULL) {
+    SDL_Log("Unable to render text surface! SDL_ttf Error: %s\n",
+            TTF_GetError());
+  } else {
+    // Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface(ren, textSurface);
+    if (newTexture == NULL) {
+      SDL_Log("Unable to create texture from rendered text! SDL Error: %s\n",
+              SDL_GetError());
+    }
+
+    w = textSurface->w;
+    h = textSurface->h;
+
+    // Get rid of old surface
+    SDL_FreeSurface(textSurface);
+  }
+
+  textMap[key] = std::make_tuple(newTexture, w, h);
+
+  return textMap[key];
+}
+
+Mix_Chunk *ResourceManager::loadEffect(const std::string &path) {
+  auto foundEffect = effectMap.find(path);
+  if (foundEffect != effectMap.end()) {
+    return foundEffect->second;
+  }
+
+  auto loadedEffect = Mix_LoadWAV(path.c_str());
+  if (loadedEffect == NULL) {
+    SDL_Log("Unable to create sound effect! Mix Error: %s\n", Mix_GetError());
+  }
+
+  effectMap[path] = loadedEffect;
+
+  return loadedEffect;
+}
+
+Mix_Music *ResourceManager::loadMusic(const std::string &path) {
+  auto foundSong = songMap.find(path);
+  if (foundSong != songMap.end()) {
+    return foundSong->second;
+  }
+
+  auto loadedSong = Mix_LoadMUS(path.c_str());
+  if (loadedSong == NULL) {
+    SDL_Log("Unable to create music! Mix Error: %s\n", Mix_GetError());
+  }
+
+  songMap[path] = loadedSong;
+
+  return loadedSong;
+}
